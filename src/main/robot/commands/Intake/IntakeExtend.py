3309e5320d30d146dc2f135e23b602632ae395e8
SathR12 <74515743+SathR12@users.noreply.github.com>
2022-04-09 19:32:34 -0400

    Add files via upload

---
+++ src/main/robot/commands/Intake/IntakeExtend.py
@@ -0,0 +1,23 @@
+import wpilib
+import commands2
+from commands2 import CommandBase
+import main.robot.Robot as Robot
+from main.robot.Robot import MyRobot
+
+class IntakeExtend(CommandBase):
+    def __init__(self):
+        self.addRequirements(MyRobot.INTAKE)
+
+    def initialize(self) -> None:
+        return super().initialize()
+
+    def execute(self):
+        MyRobot.INTAKE.intakeExtendSolenoid()
+
+    def isFinished(self):
+        return True
+
+    def end(self, interrupted: bool) -> None:
+        return super().end(interrupted)
+
+        
---
+++ src/main/util/subsystems/MechanicalSubsystem.py
@@ -0,0 +1,16 @@
+import wpilib 
+import commands2
+
+class MechanicalSubsystem(commands2.SubsystemBase):
+    def stop(self):
+        pass
+
+    def ping(self):
+        pass
+
+    def isAlive(self):
+        pass
+
+    def shuffleBoard(self):
+        pass
+
